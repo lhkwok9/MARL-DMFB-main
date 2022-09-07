a7a0360e2b1f924f4fdc9011e710290c10e7c892
lhkwok9 <1155144407@link.cuhk.edu.hk>
2022-09-07 23:32:15 +0800

    ?

--- network/base_net.py
+++ network/base_net.py
@@ -15,7 +15,7 @@ class RNN(nn.Module):
     def forward(self, obs, hidden_state):
         x = f.relu(self.fc1(obs))
         h_in = hidden_state.reshape(-1, self.args.rnn_hidden_dim)
-        h = self.rnn(x, h_in)
+        h = self.rnn(x, h_in) 
         q = self.fc2(h)
         return q, h
 

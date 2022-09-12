eeab89d336d7884f6d0012847af9adc19048a9f6
lhkwok9 <1155144407@link.cuhk.edu.hk>
2022-09-13 03:24:05 +0800

    ~85%

--- network/base_net.py
+++ network/base_net.py
@@ -79,20 +79,21 @@ class CRNN_Inception(nn.Module):
         super(CRNN_Inception, self).__init__()
         self.args = args
         # first path
-        self.conv1 = nn.Conv2d(4, 16, kernel_size=1)
+        # self.conv1 = nn.Conv2d(4, 16, kernel_size=1)
         # second path
-        self.conv2 = nn.Conv2d(4, 24, kernel_size=1)
-        self.conv2_1 = nn.Conv2d(24, 16, kernel_size=(1,2))
-        self.conv2_2 = nn.Conv2d(24, 16, kernel_size=(2,1))
+        # self.conv2 = nn.Conv2d(4, 24, kernel_size=1)
+        self.conv2_1 = nn.Conv2d(4, 32, kernel_size=(1,5))
+        self.conv2_2 = nn.Conv2d(4, 32, kernel_size=(5,1))
         # third path
-        self.conv3 = nn.Conv2d(4, 24, kernel_size=1)
-        self.conv3_1 = nn.Conv2d(24, 28, kernel_size=(1,2))
-        self.conv3_1_1 = nn.Conv2d(28, 32, kernel_size=(2,1))
-        self.conv3_1_1_1 = nn.Conv2d(32, 16, kernel_size=(2,1))
-        self.conv3_1_1_2 = nn.Conv2d(32, 16, kernel_size=(1,2))
+        # self.conv3 = nn.Conv2d(4, 24, kernel_size=1)
+        # self.conv3_1 = nn.Conv2d(4, 28, kernel_size=(1,3))
+        # self.conv3_1_1 = nn.Conv2d(28, 32, kernel_size=(3,1))
+        self.conv3_1_1_1 = nn.Conv2d(4, 32, kernel_size=(2,2))
+        self.conv3_1_1_2 = nn.Conv2d(32, 32, kernel_size=(2,2))
+        self.conv3_1_1_3 = nn.Conv2d(32, 64, kernel_size=(2,2))
 
         self.mlp1 = nn.Linear(2+args.n_agents+args.n_actions, 10)
-        self.rnn = nn.GRUCell(1424+10, args.rnn_hidden_dim)
+        self.rnn = nn.GRUCell(2*2*64+2*5*32+10, args.rnn_hidden_dim)
         self.fc1 = nn.Linear(args.rnn_hidden_dim, args.n_actions)
 
         # attention code
@@ -103,28 +104,29 @@ class CRNN_Inception(nn.Module):
             inputs, [self.args.fov*self.args.fov*4, self.args.n_agents+self.args.n_actions+2], dim=1)
         pixel = pixel.reshape((-1, 4, self.args.fov, self.args.fov))
         # first path
-        pixel_1 = f.relu(self.conv1(pixel))
+        # pixel_1 = f.relu(self.conv1(pixel))
         # second path
-        pixel_2 = f.relu(self.conv2(pixel))
-        pixel_2_1 = f.relu(self.conv2_1(pixel_2))
-        pixel_2_2 = f.relu(self.conv2_1(pixel_2))
+        # pixel_2 = f.relu(self.conv2(pixel))
+        pixel_2_1 = f.relu(self.conv2_1(pixel))
+        pixel_2_2 = f.relu(self.conv2_2(pixel))
         # thrid path
-        pixel_3 = f.relu(self.conv3(pixel))
-        pixel_3 = f.relu(self.conv3_1(pixel_3))
-        pixel_3 = f.relu(self.conv3_1_1(pixel_3))
-        pixel_3_1 = f.relu(self.conv3_1_1_1(pixel_3))
-        pixel_3_2 = f.relu(self.conv3_1_1_2(pixel_3))
+        # pixel_3 = f.relu(self.conv3(pixel))
+        # pixel_3 = f.relu(self.conv3_1(pixel_3))
+        # pixel_3 = f.relu(self.conv3_1_1(pixel_3))
+        pixel_3_1 = f.relu(self.conv3_1_1_1(pixel))
+        pixel_3_2 = f.relu(self.conv3_1_1_2(pixel_3_1))
+        pixel_3_3 = f.relu(self.conv3_1_1_3(pixel_3_2))
 
-        pixel_1 = pixel_1.reshape((-1, 5*5*16))  # fov9: (batch,800) fov5: (batch,32)
+        # pixel_1 = pixel_1.reshape((-1, 5*5*16))  # fov9: (batch,800) fov5: (batch,32)
 
-        pixel_2_1 = pixel_2_1.reshape((-1, 5*4*16))
-        pixel_2_2 = pixel_2_2.reshape((-1, 5*4*16))
+        pixel_2_1 = pixel_2_1.reshape((-1, 5*1*32))
+        pixel_2_2 = pixel_2_2.reshape((-1, 5*1*32))
         
-        pixel_3_1 = pixel_3_1.reshape((-1, 3*4*16))
-        pixel_3_2 = pixel_3_2.reshape((-1, 3*4*16))
+        pixel_3_3 = pixel_3_3.reshape((-1, 2*2*64))
+        # pixel_3_2 = pixel_3_2.reshape((-1, 3*3*16))
 
         vec = f.relu(self.mlp1(vec))  # (batch,10)
-        x = torch.cat([pixel_1, pixel_2_1, pixel_2_2, pixel_3_1, pixel_3_2, vec], dim=1)  # fov9: (batch,810) fov5: (batch,42)
+        x = torch.cat([pixel_2_1, pixel_2_2, pixel_3_3, vec], dim=1)  # fov9: (batch,810) fov5: (batch,42)
         h_in = hidden_state.reshape(-1, self.args.rnn_hidden_dim)
         h = self.rnn(x, h_in)
 
